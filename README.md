08020f0dd360234c27da83d1fb858ee6e22282d4
ServandoValdez <servando.valdez216775@potros.itson.edu.mx>
2022-02-16 20:02:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The-Cheezery-
+Aplicación creado en android studio por Servando Valdez, esta aplicación contiene un login y un menu
---
+++ app/src/main/java/servando/valdez/thecheezery/MenuActivity.kt
@@ -0,0 +1,14 @@
+package servando.valdez.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+    }
+}
