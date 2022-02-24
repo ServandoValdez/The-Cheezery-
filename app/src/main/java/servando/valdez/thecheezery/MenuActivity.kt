2b9c8fae815766754f751c8fd4c02496bcac2b87
ServandoValdez <servando.valdez216775@potros.itson.edu.mx>
2022-02-24 11:10:18 -0700

    tercer commit

--- app/src/main/java/servando/valdez/thecheezery/MenuActivity.kt
+++ app/src/main/java/servando/valdez/thecheezery/MenuActivity.kt
@@ -11,10 +11,33 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
-
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweets: Button = findViewById(R.id.button_sweets) as Button
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
         btnCold.setOnClickListener{
             var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","coldDrinks")
             startActivity(intent)
         }
+
+        btnHot.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","hotDrinks")
+            startActivity(intent)
+        }
+
+        btnSalties.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","salties")
+            startActivity(intent)
+        }
+
+        btnSweets.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType","sweets")
+            startActivity(intent)
+        }
+
+
     }
 }
